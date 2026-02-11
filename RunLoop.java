ad38423ad8549f3b6bf22b2b3e145983a84dd7ad
Kanani Het <tech.kananihet@gmail.com>
2026-02-11 22:29:50 +0530

    Reason written why loop run infinite times

--- RunLoop.java
+++ RunLoop.java
@@ -5,5 +5,9 @@ public class RunLoop {
             System.out.println("Apna College");
         }
 
+        // this loop runs infinite time because there is no condition to stop it and there is 
+        // no increment or decrement statement to change the value of any variable. 
+        // It will keep printing "Apna College" indefinitely.
+
     }
 }
