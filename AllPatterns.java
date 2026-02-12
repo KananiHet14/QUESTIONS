7614b8e805316c5fb6d314cc1ae2c6ee9bbb9415
Kanani Het <tech.kananihet@gmail.com>
2026-02-12 14:40:52 +0530

    4 advanced patterns

--- AllPatterns.java
+++ AllPatterns.java
@@ -210,7 +210,42 @@ public class AllPatterns {
             //     System.out.println();
             // }
 
+            
+            
+            // number pyramid
+            // int n=5;
 
+            // for(int i=1; i<=n; i++) {
+            //     // spaces
+            //     for(int j=1; j<=n-i; j++) {
+            //         System.out.print(" ");
+            //     }
+
+            //     for(int j=1; j<=i; j++) {
+            //         System.out.print(j + " ");
+            //     }
+            //     System.out.println();
+            // }
+
+
+            // pyldromic pyramid
+            // int n=5;
+
+            // for(int i=1; i<=n; i++) {
+            //     // spaces
+            //     for(int j=1; j<=n-i; j++) {
+            //         System.out.print(" ");
+            //     }
+            //     // first part
+            //     for(int j=i; j>=1; j--) {
+            //         System.out.print(j);
+            //     }
+            //     // second part
+            //     for(int j=2; j<=i; j++) {
+            //         System.out.print(j);
+            //     }
+            //     System.out.println();
+            // }
 
 
         }
