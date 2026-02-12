df8b5cc8a80b9fb99360c6ebf4e2e48c92e47621
Kanani Het <tech.kananihet@gmail.com>
2026-02-12 21:54:19 +0530

    all pattern's problems solved

--- AllPatterns.java
+++ AllPatterns.java
@@ -282,5 +282,39 @@ public class AllPatterns {
             // }
 
 
+            // daimond pattern
+            // int n=5;
+
+            // for(int i=1; i<=n; i++) {
+            //     // spaces
+            //     for(int j=1; j<=n-i; j++) {
+            //         System.out.print(" ");
+            //     }
+            //     // first part
+            //     for(int j=i; j>=1; j--) {
+            //         System.out.print("*");
+            //     }
+            //     // second part
+            //     for(int j=2; j<=i; j++) {
+            //         System.out.print("*");
+            //     }
+            //     System.out.println();
+            // }
+            // for(int i=n; i>=1; i--) {
+            //     // spaces
+            //     for(int j=1; j<=n-i; j++) {
+            //         System.out.print(" ");
+            //     }
+            //     // inverted first part
+            //     for(int j=i; j>=1; j--) {
+            //         System.out.print("*");
+            //     }
+            //     // inverted second part
+            //     for(int j=2; j<=i; j++) {
+            //         System.out.print("*");
+            //     }
+            //     System.out.println();
+            // }
+
         }
 }
